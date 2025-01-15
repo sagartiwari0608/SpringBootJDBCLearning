0e06be4f882a28a82381430258eec5ad4da5114f
sagartiwari0608 <sagartiwarisrt@gmail.com>
2025-01-15 10:36:49 +0530

    initial commit to add files to github for jdbc learning

---
+++ src/main/java/com/springboot/JDBCLearning/JdbcLearningApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.JDBCLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcLearningApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(JdbcLearningApplication.class, args);
+	}
+
+}
